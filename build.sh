3f1a16f38bf90cd4ddf6ec1bf8e88adc2717ea77
Argo Roots <argo@roots.ee>
2016-12-30 13:36:32 +0200

    Travis deploy to gh-pages

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o errexit -o nounset
+
 npm install entu-cms
 
 rm -r ./tmp
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+cd docs
+
+git init
+git config user.name "Argo Roots"
+git config user.email "argo@roots.ee"
+
+git remote add upstream "https://$GH_TOKEN@github.com/argoroots/polska.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+# echo "polska.ee" > CNAME
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages

47ce0b00c7bf1c9313008fb95a501ccb27ad6514
Argo Roots <argo@roots.ee>
2020-01-03 16:35:07 +0200

    Upgrade to Entu SSG

--- build.sh
+++ build.sh
@@ -2,20 +2,7 @@
 
 # set -o errexit -o nounset
 
-npm install entu-cms
+ENTU_QUERY="_type.string=profile&props=access.string,audio.string,author.string,connection.string,crafter.string,flowers.string,idea.string,info.string,location.string,more_info.string,name.string,opening_attendee.string,opening.string,ownership.string,path.string,photo._id,photo_text.string,trivia.string" ./node_modules/entu-ssg/helpers/entu2yaml.js ./source/item/data.yaml
 
-rm -r ./tmp_source
-mkdir ./tmp_source
-cp -r ./source/* ./tmp_source
-
-ENTU_URL=https://poola.entu.ee \
-OUT_DIR=./tmp_source \
-E_DEF=profile \
-TEMPLATE=./source/item.jade \
-DATA_LIST=./tmp_source/entities.yaml \
-./node_modules/entu-cms/helpers/entu2yaml.js && \
-rm -rf ./build
-
-./node_modules/entu-cms/build.js ./entu-cms.yaml
-
+npm run build
 cp -r ./assets/ ./build/assets

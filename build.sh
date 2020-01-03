2e1447055611cd5dd81f9473f62774fe6028168d
Argo Roots <argo@roots.ee>
2020-01-03 16:42:05 +0200

    Import geo and google_map data

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # set -o errexit -o nounset
 
-ENTU_QUERY="_type.string=profile&props=access.string,audio.string,author.string,connection.string,crafter.string,flowers.string,idea.string,info.string,location.string,more_info.string,name.string,opening_attendee.string,opening.string,ownership.string,path.string,photo._id,photo_text.string,trivia.string" ./node_modules/entu-ssg/helpers/entu2yaml.js ./source/item/data.yaml
+ENTU_QUERY="_type.string=profile&props=access.string,audio.string,author.string,connection.string,crafter.string,flowers.string,geo.string,google_maps.string,idea.string,info.string,location.string,more_info.string,name.string,opening_attendee.string,opening.string,ownership.string,path.string,photo._id,photo_text.string,trivia.string" ./node_modules/entu-ssg/helpers/entu2yaml.js ./source/item/data.yaml
 
 npm run build
 cp -r ./assets/ ./build/assets
